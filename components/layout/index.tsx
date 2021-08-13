564ab96b204f952b39295f9a9031043a70bddead
Dea Sunara <deasunara@gmail.com>
2021-08-13 22:49:54 +0700

    custom halaman 404 not found page

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -4,14 +4,14 @@ import Footer from "../Footer";
 import styles from "./layout.module.css"
 
 interface LayoutProps {
-    Children: ReactNode;
+    children: ReactNode;
 }
 export default function Layout(props: LayoutProps) {
-  const { Children } = props;
+  const { children } = props;
   return (
     <div className={styles.container}>
       <Header />
-      <div className={styles.content}>{Children}</div>
+      <div className={styles.content}>{children}</div>
       {/* <div className={styles.content}>
         <h1 className={styles.content}>selamat datang</h1>
       </div> */}
